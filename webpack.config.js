6db22229195e5f55401330fe77767c34f0212587
jpalu5150 <119819587+jpalu5150@users.noreply.github.com>
2023-05-02 17:37:32 -0700

    Intitial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname,
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        loader: "babel",
+        query: {
+          presets: ["react", "env"],
+          plugins: ["transform-class-properties"]
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules"],
+  },
+  resolveLoader: {
+    moduleExtensions: ["-loader"],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "./",
+  },
+};
